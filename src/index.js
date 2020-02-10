f00c949eb760a15f6221a9687c2c5c09363c2389
Amr Salama <amr.mo.salama@gmail.com>
2020-02-11 00:06:27 +0200

    feat: basic steps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const rp = require('request-promise');
+
+const parsedSheets = require('./sheets.json');
 
 const { PORT = 5000 } = process.env;
 
@@ -8,6 +11,43 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/parse', async (req, res) => {
+  const traineesListJSONUrl = req.query['trainees-list'];
+  const sheetsListJSONUrl = req.query['sheets-list'];
+
+  // check links existence
+  if (!traineesListJSONUrl || !sheetsListJSONUrl) {
+    return res.status(400).json({
+      message: 'trainees-list or sheets-list query not found'
+    });
+  }
+
+  // start fetch json data
+  let trainees, sheets;
+  try {
+    const response = await Promise.all([
+      rp.get({ uri: traineesListJSONUrl, json: true }),
+      rp.get({ uri: sheetsListJSONUrl, json: true })
+    ]);
+    [trainees, sheets] = response;
+  } catch (error) {
+    return res.status(400).json({
+      message: 'invalid trainees-list or sheets-list urls'
+    });
+  }
+
+  // response
+  const response = {
+    trainees: [],
+    sheets: [],
+    submissions: {}
+  };
+  return res.json(response);
+});
+
+app.get('*', (req, res) => {
+  res.status(404);
+  res.json({ message: 'not found' });
+});
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
