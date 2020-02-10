9080ab1ff9938594fdf9a4c697a0c72ea287fd1b
Amr Salama <amr.mo.salama@gmail.com>
2020-02-10 23:40:16 +0200

    feat: simple server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('a');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { PORT = 5000 } = process.env;
+
+const app = express();
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
